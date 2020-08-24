f39824d328bb029ce0c1117325d30808b05494f1
Md. Azharul Islam Somon <sumon.gsc14@gmail.com>
2020-08-24 23:13:23 +0800

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
